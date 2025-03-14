0ac3e14774e46ffb4c43d9e5c9887bd1306079cd
Felipe Fritz Pessoa <fritzlumen@hotmail.com>
2025-03-14 13:18:31 -0300

    Implementacao do get dos clientes

--- src/pages/Clientes/index.js
+++ src/pages/Clientes/index.js
@@ -1,21 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./styles.css";
+import api from "../../services/api";
 import logoCadastro from "../../assets/cadastro.png";
 import { FiEdit, FiUserX, FiXCircle } from "react-icons/fi";
 
 export default function Clientes() {
+  //const [nome, setNome] = useState("");
+  const [clientes, setClientes] = useState([]);
+
+  const email = localStorage.getItem("email");
+  const token = localStorage.getItem("token");
+
+  const navigate = useNavigate();
+
+  const authorization = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  useEffect(() => {
+    api.get("api/Clientes", authorization).then((response) => {
+      setClientes(response.data);
+    }, token);
+  });
+
+  async function logout() {
+    try {
+      localStorage.clear();
+      localStorage.setItem("token", '');
+      authorization.headers = '';
+      navigate("/");
+    } catch (error) {
+      alert("Falha no logout, tente novamente. " + error.message);
+    }
+  }
+
   return (
     <div className="cliente-container">
       <header>
         <img src={logoCadastro} alt="Cadastro" />
         <span>
-          Bem vindo, <strong>Fritas</strong>
+          Bem vindo, <strong>{email}</strong>
         </span>
         <Link className="button" to="/cliente/novo/0">
           Novo Cliente
         </Link>
-        <button type="button">
+        <button onClick={logout} type="button">
           <FiXCircle size={35} color="#17202a" />
         </button>
       </header>
@@ -27,28 +59,22 @@ export default function Clientes() {
       </form>
       <h1>Relação de Clientes</h1>
       <ul>
-      <li>
-          <b>Nome:</b>Liz<br /><br />
-          <b>Email:</b>lizpililica@gmail.com<br /><br />
-          <b>Idade:</b>5<br /><br />
-          <button type="button">
-            <FiEdit size={20} color="#17202a" />
-          </button>
-          <button type="button">
-            <FiUserX size={20} color="#17202a" />
-          </button>
-        </li>
-        <li>
-          <b>Nome:</b>Iza<br /><br />
-          <b>Email:</b>izatililica@gmail.com<br /><br />
-          <b>Idade:</b>4<br /><br />
-          <button type="button">
-            <FiEdit size={20} color="#17202a" />
-          </button>
-          <button type="button">
-            <FiUserX size={20} color="#17202a" />
-          </button>
-        </li>
+        {clientes.map((cliente) => (
+          <li key={cliente.id}>
+            <strong>Nome:</strong>
+            <p>{cliente.nome}</p>
+            <strong>Email:</strong>
+            <p>{cliente.email}</p>
+            <strong>Idade:</strong>
+            <p>{cliente.idade}</p>
+            <button type="button">
+              <FiEdit size={20} color="#17202a" />
+            </button>
+            <button type="button">
+              <FiUserX size={20} color="#17202a" />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
